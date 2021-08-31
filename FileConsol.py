cf211387daf9a7143fe7d507b852364e8cd16be7
Liu373 <87485446+Liu373@users.noreply.github.com>
2021-08-30 20:44:27 -0400

    Update FileConsol.py

--- FileConsol.py
+++ FileConsol.py
@@ -1,1 +1,46 @@
+import pandas as pd
+import os
+import re
+import time
+
+
+class File_Loop:
+  
+  def __init__(self, path, save_location):
+    self.path = path
+    self.save_location = save_location
+  
+  
+  def _processing(self):
+    FileConsol = pd.DataFrame()
+    
+    for csv in os.listdir(self.path):
+      
+      if re.match("xxx_xxx_
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
